3cf92521d4b7306ce92d66f4ae5215b08a1d174d
Vinicius <viniciusbsouto@gmail.com>
2025-10-08 15:20:23 -0300

    feat: add prettier and eslint setup

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,9 @@
 import js from '@eslint/js'
-import globals from 'globals'
+import eslintConfigPrettier from 'eslint-config-prettier'
 import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import globals from 'globals'
 
 export default [
   { ignores: ['dist'] },
@@ -22,6 +23,7 @@ export default [
       react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      'simple-import-sort': simpleImportSort,
     },
     rules: {
       ...js.configs.recommended.rules,
@@ -29,10 +31,8 @@ export default [
       ...react.configs['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
       'react/jsx-no-target-blank': 'off',
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      'react/prop-types': 'off',
     },
   },
+  eslintConfigPrettier,
 ]
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 const App = () => {
+  const name = 'Vinicius';
   return (
     <div>
       <h1>Hello World</h1>
     </div>
   );
-}
- 
+};
+
 export default App;
