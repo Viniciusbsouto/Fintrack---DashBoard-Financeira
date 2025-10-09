abd2819fbdbe69c219d10b6bb37e840c11213e1f
Vinicius <viniciusbsouto@gmail.com>
2025-10-09 00:49:23 -0300

    chore: add shadcn setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import { Button } from './components/ui/button';
+
 const App = () => {
   return (
     <div>
-      <h1 className="my-2 p-5 text-red-500">Hello World</h1>
+      <Button variant="default">Default Button</Button>
     </div>
   );
 };
