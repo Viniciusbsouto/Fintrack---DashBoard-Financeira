7e2c1f9eb9c9a4c12c27beefdd725513927f2f3a
Vinicius <viniciusbsouto@gmail.com>
2025-10-10 16:12:05 -0300

    feat: add api integration on sign up page

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -1,6 +1,9 @@
 import { zodResolver } from '@hookform/resolvers/zod';
+import { useMutation } from '@tanstack/react-query';
+import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { Link } from 'react-router';
+import { toast } from 'sonner';
 import { z } from 'zod';
 
 import PasswordInput from '@/components/password-input';
@@ -23,6 +26,7 @@ import {
   FormMessage,
 } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
+import { api } from '@/lib/axios';
 
 const signupSchema = z.object({
   firstName: z.string().trim().min(2, { message: 'O nome é obrigatório' }),
@@ -45,6 +49,20 @@ const signupSchema = z.object({
 });
 
 const SignUpPage = () => {
+  const [user, setUser] = useState(null);
+  const signupMutation = useMutation({
+    mutationKey: ['signup'],
+    mutationFn: async (variables) => {
+      const response = await api.post('/users', {
+        first_name: variables.firstName,
+        last_name: variables.lastName,
+        email: variables.email,
+        password: variables.password,
+      });
+      return response.data;
+    },
+  });
+
   const methods = useForm({
     resolver: zodResolver(signupSchema),
     defaultValues: {
@@ -56,9 +74,27 @@ const SignUpPage = () => {
       terms: false,
     },
   });
+
   const handleSubmit = (data) => {
-    console.log(data);
+    signupMutation.mutate(data, {
+      onSuccess: (createdUser) => {
+        const accessToken = createdUser.tokens.accessToken;
+        const refreshToken = createdUser.tokens.refreshToken;
+        setUser(createdUser);
+        localStorage.setItem('accessToken', accessToken);
+        localStorage.setItem('refreshToken', refreshToken);
+        toast.success('Conta criada com sucesso!');
+      },
+      onError: () => {
+        toast.error('Erro ao criar conta. Tente novamente.');
+      },
+    });
   };
+
+  if (user) {
+    return <h1>Olá, {user.first_name}</h1>;
+  }
+
   return (
     <div className="flex h-screen w-screen flex-col items-center justify-center gap-3">
       <Form {...methods}>
